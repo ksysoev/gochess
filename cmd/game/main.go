ad90a2b66d5d83b6ab8515a97125a39cd37e04a5
Кирилл Сысоев <k.sysoev@me.com>
2023-06-04 12:00:54 +0800

    Redesign api for game server

--- cmd/game/main.go
+++ cmd/game/main.go
@@ -2,11 +2,25 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/notnil/chess"
 )
+
+type GameRepoStorage struct {
+	CurrentGameID int
+	Postitions    map[string]string
+}
+
+var GameRepo GameRepoStorage = GameRepoStorage{
+	CurrentGameID: 0,
+	Postitions:    make(map[string]string),
+}
 
 type MoveRequest struct {
 	Move string `json:"move"`
@@ -16,23 +30,52 @@ type MoveResponse struct {
 	Position string `json:"position"`
 }
 
-var currentGame *chess.Game
+type StartGameResponse struct {
+	ID       string `json:"id"`
+	Position string `json:"position"`
+}
 
 func startGame(w http.ResponseWriter, r *http.Request) {
 	// Start a new game
-	currentGame = chess.NewGame()
+	newGame := chess.NewGame()
+
+	GameRepo.CurrentGameID += 1
+	id := fmt.Sprintf("%d", GameRepo.CurrentGameID)
+	GameRepo.Postitions[id] = newGame.Position().String()
+
+	resp := StartGameResponse{
+		ID:       id,
+		Position: newGame.Position().String(),
+	}
+
+	res, err := json.Marshal(resp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
 	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(res)
 }
 
 func getGame(w http.ResponseWriter, r *http.Request) {
-	if currentGame == nil {
-		http.Error(w, "Game not started", http.StatusBadRequest)
+	gameID := chi.URLParam(r, "gameID")
+
+	if gameID == "" {
+		http.Error(w, "Game ID is required", http.StatusBadRequest)
+		return
+	}
+
+	gamePosition, ok := GameRepo.Postitions[gameID]
+
+	if !ok {
+		http.Error(w, "Game not started or finished", http.StatusBadRequest)
 		return
 	}
 
 	resp := MoveResponse{
-		Position: currentGame.Position().String(),
+		Position: gamePosition,
 	}
 
 	res, err := json.Marshal(resp)
@@ -48,24 +91,23 @@ func getGame(w http.ResponseWriter, r *http.Request) {
 }
 
 func finishGame(w http.ResponseWriter, r *http.Request) {
-	if currentGame == nil {
-		http.Error(w, "Game not started", http.StatusBadRequest)
+	gameID := chi.URLParam(r, "gameID")
+
+	if gameID == "" {
+		http.Error(w, "Game ID is required", http.StatusBadRequest)
 		return
 	}
 
-	currentGame = nil
+	delete(GameRepo.Postitions, gameID)
 
 	w.WriteHeader(http.StatusOK)
 }
 
 func move(w http.ResponseWriter, r *http.Request) {
-	if currentGame == nil {
-		http.Error(w, "Game not started", http.StatusBadRequest)
-		return
-	}
+	gameID := chi.URLParam(r, "gameID")
 
-	if r.Method != http.MethodPost {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	if gameID == "" {
+		http.Error(w, "Game ID is required", http.StatusBadRequest)
 		return
 	}
 
@@ -75,13 +117,29 @@ func move(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if err := currentGame.MoveStr(req.Move); err != nil {
+	gamePosition, ok := GameRepo.Postitions[gameID]
+
+	if !ok {
+		http.Error(w, "Game not started or finished", http.StatusBadRequest)
+		return
+	}
+
+	fen, err := chess.FEN(gamePosition)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	game := chess.NewGame(fen)
+
+	if err := game.MoveStr(req.Move); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
 	resp := MoveResponse{
-		Position: currentGame.Position().String(),
+		Position: game.Position().String(),
 	}
 
 	res, err := json.Marshal(resp)
@@ -98,17 +156,23 @@ func move(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	// Create a new ServeMux
-	mux := http.NewServeMux()
+	r := chi.NewRouter()
 
 	// Register the /start, /move, and /finish routes
-	mux.HandleFunc("/start", startGame)
-	mux.HandleFunc("/move", move)
-	mux.HandleFunc("/finish", finishGame)
-	mux.HandleFunc("/game", getGame)
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Timeout(5 * time.Second))
+
+	r.Post("/game", startGame)
+	r.Get("/game/{gameID}", getGame)
+	r.Delete("/game/{gameID}", finishGame)
+	r.Post("/game/{gameID}/move", move)
 
 	// Serve the routes using the ServeMux
 	log.Println("Starting Game Server on port 8080")
-	if err := http.ListenAndServe(":8080", mux); err != nil {
+	if err := http.ListenAndServe(":8080", r); err != nil {
 		log.Fatal(err)
 	}
 }
