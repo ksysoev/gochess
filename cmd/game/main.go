d7633553c8e6b4a833c9f82f196a5fb9b619e1c9
Кирилл Сысоев <k.sysoev@me.com>
2023-06-04 21:56:30 +0800

    Creates dummy match making

--- cmd/game/main.go
+++ cmd/game/main.go
@@ -153,6 +153,63 @@ func move(w http.ResponseWriter, r *http.Request) {
 	w.Write(res)
 }
 
+var queue []string = make([]string, 0)
+
+type MatchRequest struct {
+	Name string `json:"name"`
+}
+
+type MatchResponse struct {
+	White  string `json:"white,omitempty"`
+	Black  string `json:"black,omitempty"`
+	Status string `json:"status"`
+	GameID string `json:"game_id,omitempty"`
+}
+
+func findMatch(w http.ResponseWriter, r *http.Request) {
+	var req MatchRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if req.Name == "" {
+		http.Error(w, "Name is required", http.StatusBadRequest)
+		return
+	}
+
+	var resp MatchResponse
+	if len(queue) == 0 {
+		queue = append(queue, req.Name)
+		resp = MatchResponse{
+			Status: "pending",
+		}
+	} else {
+		white := queue[0]
+		queue = queue[1:]
+		black := req.Name
+		id := uuid.New().String()
+		GameRepo.Postitions[id] = newGame.Position().String()
+
+		resp = MatchResponse{
+			Status: "ready",
+			White:  white,
+			Black:  black,
+			GameID: id,
+		}
+	}
+
+	res, err := json.Marshal(resp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(res)
+}
+
 func main() {
 	// Create a new ServeMux
 	r := chi.NewRouter()
