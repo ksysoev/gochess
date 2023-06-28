2c50c2653d4c5ad6880cf42f3b13fa8d73b77d74
Кирилл Сысоев <k.sysoev@me.com>
2023-06-28 22:20:24 +0800

    learning vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,68 +1,19 @@
 <template>
-  <div>
-    <div v-if="!showGameView">
-      <label for="username">
-        <input
-          placeholder="Enter your username"
-          type="text"
-          id="username"
-          v-model="username"/>
-      </label>
-      <button @click="findMatch">Find a match</button>
-    </div>
-    <GameView v-if="showGameView" :white="white" :black="black" />
-  </div>
+  <HomePage />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { APIConfig } from '@/api/config';
-import GameView from './components/GameView.vue';
+import HomePage from './components/HomePage.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    GameView,
-  },
-  data() {
-    return {
-      username: '',
-      white: '',
-      black: '',
-      showGameView: false,
-    };
-  },
-  methods: {
-    async findMatch() {
-      try {
-        const response = await fetch(`${APIConfig.baseURL}/match/`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ name: this.username }),
-        });
-        const data = await response.json();
-
-        console.log(data);
-        this.white = data.white;
-        this.black = data.black;
-        this.showGameView = true;
-      } catch (error) {
-        console.error(error);
-      }
-    },
+    HomePage,
   },
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ frontend/src/components/HomePage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="px-4 py-5 my-5 text-center">
+    <h1 class="display-5 fw-bold text-body-emphasis">Welcome to Go Chess</h1>
+    <div class="col-lg-6 mx-auto">
+      <p class="lead mb-4">
+        Get ready to test your strategic skills and start playing now.
+        Click the button below to begin your chess journey!
+      </p>
+      <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+        <button type="button" class="btn btn-primary btn-lg px-4 gap-3">Start Game</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HomePage',
+});
+</script>
+
+<style>
+</style>
